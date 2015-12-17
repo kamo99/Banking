aba2efea33db16c4c9e5613d3d6dcd4c7fb80b2a
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-12-17 09:02:10 -0500

    Implement bubblesort

---
+++ SortAll.java
@@ -0,0 +1,17 @@
+class SortAll {
+	public static Comparable[] bubble(Comparable[] comparables) {
+		boolean isSorted = false;
+		while (!isSorted) {
+			isSorted = true; // assume that it is sorted until proven otherwise
+			for (int i = 0; i < comparables.length - 1; i++) {
+				if (comparables[i].compareTo(comparables[i+1]) > 0) {
+					Comparable aux = comparables[i];
+					comparables[i] = comparables[i+1];
+					comparables[i+1] = aux;
+					isSorted = false;
+				}
+			}
+		}
+		return comparables;
+	}
+}
