a9ddc0a81e85a94af40a0bc84aaf8a31476718ca
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-12-17 21:55:16 -0500

    Enable reverse direction sorting

--- SortAll.java
+++ SortAll.java
@@ -1,10 +1,13 @@
 class SortAll {
-	public static Comparable[] bubble(Comparable[] comparables) {
+	// by default, it sorts least to greatest
+	// to make it greatest to least, pass in direction as a negative number
+	public static Comparable[] bubble(Comparable[] comparables, int direction) {
+		direction = direction / Math.abs(direction);
 		boolean isSorted = false;
 		while (!isSorted) {
 			isSorted = true; // assume that it is sorted until proven otherwise
 			for (int i = 0; i < comparables.length - 1; i++) {
-				if (comparables[i].compareTo(comparables[i+1]) > 0) {
+				if (comparables[i].compareTo(comparables[i+1]) * direction > 0) {
 					Comparable aux = comparables[i];
 					comparables[i] = comparables[i+1];
 					comparables[i+1] = aux;
@@ -13,5 +16,9 @@ class SortAll {
 			}
 		}
 		return comparables;
+	}
+
+	public static Comparable[] bubble(Comparable[] comparables) {
+		return bubble(comparables, 1);
 	}
 }
